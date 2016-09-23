767f1838f47b39b3528e417132da15bc00d2e408
PrashanthYP <ypprashanth@gmail.com>
2016-09-23 20:03:50 +0530

    changes in the server confit file

--- web/client/app/config.js
+++ web/client/app/config.js
@@ -1,7 +1,7 @@
 let projectConfig = {
   name: "Parker PooF",
-  api_base_url: "https://localhost:3002",
-  ws_url: "wss://localhost:3002",
+  api_base_url: "http://localhost:3002",
+  ws_url: "ws://localhost:3002/ws",
   auth_token : 'Bearer testAuthToken' 
 
   //api_base_url: "http://localhost:3002",
